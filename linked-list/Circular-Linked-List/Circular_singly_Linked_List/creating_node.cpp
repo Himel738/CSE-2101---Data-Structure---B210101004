ad446e2d3d625caa2f9ddfa5b28ba598aa38c0f1
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-07-06 13:23:57 +0600

    Update creating_node.cpp

--- linked-list/Circular-Linked-List/Circular_singly_Linked_List/creating_node.cpp
+++ linked-list/Circular-Linked-List/Circular_singly_Linked_List/creating_node.cpp
@@ -1,1 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+struct node {
+    struct node * link;
+    int data;
+
+};
+
+int main ()
+{
+    struct node * head = (struct node *) malloc(sizeof(struct node));
+
+    head -> data = 45;
+    head -> link = head;
+
+    printf("%d\n",head -> data);
+}
