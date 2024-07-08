11ec0e7e7380b1af2ef3e929440762352e5c7e46
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-07-08 17:15:33 +0600

    Create delete_the_first_node.cpp

---
+++ linked-list/Circular-Linked-List/Circular_singly_Linked_List/delete_the_first_node.cpp
@@ -0,0 +1,57 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node * link;
+};
+
+struct node * create(struct node * head,int data)
+{
+    head-> link = head;
+    head -> data = data;
+
+    return head;
+};
+struct node * add_end(struct node * tail,struct node * head,int data)
+{
+    struct node * nodes = (struct node *) malloc(sizeof(struct node));
+
+    nodes -> data = data;
+    tail -> link = nodes;
+    nodes -> link = head;
+
+    return nodes;
+};
+struct node * delete_first_node(struct node * head,struct node * tail)
+{
+    struct node *ptr;
+    tail -> link = head -> link;
+    ptr = head -> link;
+    free(head);
+    head -> link = NULL;
+
+    return ptr;
+};
+int main ()
+{
+    struct node * head = (struct node *) malloc(sizeof(struct node));
+    struct node * tail = create(head,45);
+
+    tail = add_end(tail,head,50);
+    tail = add_end(tail,head,60);
+    tail = add_end(tail,head,70);
+
+    head = delete_first_node(head,tail);
+
+    tail=tail -> link;
+
+    do
+    {
+        printf("%d\n",tail-> data);
+        tail = tail -> link;
+    }
+    while( tail != head);
+
+}
