084571abedba5a7a1183fa3ab66ee316d5e03c0f
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-07-08 16:48:31 +0600

    Create insertion_in_the_middle.cpp

---
+++ linked-list/Circular-Linked-List/Circular_singly_Linked_List/insertion_in_the_middle.cpp
@@ -0,0 +1,69 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    int data;
+    struct node * link;
+};
+
+struct node * create(struct node * head,int data)
+{
+    head-> link = head;
+    head -> data = data;
+
+    return head;
+};
+struct node * add_end(struct node * tail,struct node * head,int data)
+{
+    struct node * nodes = (struct node *) malloc(sizeof(struct node));
+
+    nodes -> data = data;
+    tail -> link = nodes;
+    nodes -> link = head;
+
+    return nodes;
+};
+
+struct node *add_midle(struct node * head,int data,int position,struct node * tail)
+{
+    struct node * nodes = (struct node *) malloc(sizeof(struct node));
+    nodes -> data = data;
+    nodes -> link = NULL;
+    int count = 0;
+
+    do
+    {
+        count++;
+        if(count==position)
+        {
+            nodes -> link = head -> link;
+            head -> link = nodes;
+            break;
+        }
+        head = head -> link;
+    }
+    while(head != tail);
+};
+
+int main ()
+{
+    struct node * head = (struct node *) malloc(sizeof(struct node));
+    struct node * tail = create(head,45);
+
+    tail = add_end(tail,head,50);
+    tail = add_end(tail,head,60);
+    tail = add_end(tail,head,70);
+
+    add_midle(head,100,2,tail);
+
+    tail=tail -> link;
+
+    do
+    {
+        printf("%d\n",tail-> data);
+        tail = tail -> link;
+    }
+    while( tail != head);
+
+}
