76a75b15900fb05560a5bcc68e5fdc705ab3f54f
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-07-14 15:12:41 +0600

    Create insertion_at_the_begining.cpp

---
+++ linked-list/Circular-Linked-List/Circular_doubly_Linked_List/insertion_at_the_begining.cpp
@@ -0,0 +1,51 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    struct node * prev;
+    int data;
+    struct node * next;
+
+};
+
+struct node * create_node (struct node * head,int data)
+{
+    head -> prev = head;
+    head -> next = head;
+    head -> data = data;
+};
+struct node * add_begining(struct node * tail,struct node * head,int data)
+{
+    struct node * nodes = (struct node *) malloc(sizeof(struct node));
+    nodes -> data = data;
+
+    nodes -> prev = head;
+    nodes-> next = tail;
+
+    tail -> prev = nodes;
+    head -> next = nodes;
+
+    return nodes;
+};
+
+int main ()
+{
+    struct node * head = (struct node *) malloc(sizeof(struct node));
+
+    struct node * tail = create_node(head,45);
+
+    tail = add_begining(tail,head,50);
+    tail = add_begining(tail,head,60);
+    tail = add_begining(tail,head,70);
+    tail = add_begining(tail,head,80);
+    head = tail;
+
+    do
+    {
+        printf("%d\n",tail -> data);
+        tail = tail -> next;
+    }
+
+    while(tail != head);
+}
