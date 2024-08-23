bdbd71f1d041444552667aafd1758700a81ec70e
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-08-23 18:59:43 +0600

    Create array_implementation_p1.cpp

---
+++ Stacks/array_implementation_p1.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#define MAX 4
+
+int stack_arr[MAX];
+int top = -1;
+
+void push(int data)
+{
+    if(top==MAX-1)
+    {
+        printf("Stack Overflow\n");
+        return;
+    }
+    top++;
+    stack_arr[top] = data;
+}
+
+int main ()
+{
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(4);
+
+}
