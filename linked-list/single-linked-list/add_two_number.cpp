59d8f4bdb8db3d6b8f3e1b6f0f8cca1d37d9a6f9
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-08-23 17:40:07 +0600

    Update add_two_number.cpp

--- linked-list/single-linked-list/add_two_number.cpp
+++ linked-list/single-linked-list/add_two_number.cpp
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
 struct node
 {
     struct node * link;
@@ -12,7 +11,7 @@ void print(struct node * head)
 {
     while(head != NULL)
     {
-        printf("%d\n",head->data);
+        printf("%d",head->data);
         head = head -> link;
     }
 }
@@ -52,12 +51,53 @@ struct node * reversell(struct node * head)
     }
     return head;
 }
+struct node * add(struct node * head , struct node * head2)
+{
+    struct node * ptr1 = head;
+    struct node * ptr2 = head2;
+    struct node * head3 = NULL;
+
+    int sum = 0,carry = 0;
+
+    while(ptr1 != NULL || ptr2 != NULL)
+    {
+        sum = 0;
+        if(ptr1 != NULL)
+        {
+            sum += ptr1 -> data;
+        }
+        if(ptr2 != NULL)
+        {
+            sum += ptr2 -> data;
+        }
+        sum += carry;
+        carry = sum / 10;
+        sum = sum % 10;
+
+        head3 = add_node(head3,sum);
+        if(ptr1)
+        {
+          ptr1 = ptr1 -> link;
+        }
+        if(ptr2)
+        {
+           ptr2 = ptr2 -> link;
+        }
+    }
+    if(carry)
+    {
+        head3 = add_node(head3,carry);
+    }
+
+    return head3;
+};
 int main ()
 {
     int x,y;
     scanf("%d%d",&x,&y);
 
     struct node * head = NULL;
+    struct node * head3 = NULL;
     head = create_number(x,head);
 
     struct node * head2 = NULL;
@@ -65,12 +105,9 @@ int main ()
 
     head = reversell(head);
     head2 = reversell(head2);
+    head3 = add(head,head2);
 
-    printf("First Number\n");
-    print(head);
 
-    printf("second Number\n");
-    print(head2);
-
-    print(head);
+    printf("After Adding Number\n");
+    print(head3);
 }
