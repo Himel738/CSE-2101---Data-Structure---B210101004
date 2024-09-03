f91be8f967e8eaa5729255b0658f924a460f4c89
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-09-03 14:32:01 +0600

    Create linked_list_representation_binary_tree.cpp

---
+++ Tree/Binary_Tree/linked_list_representation_binary_tree.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    struct node *left;
+    int data;
+    struct node *right;
+};
+
+struct node *create_tree()
+{
+    int input,data;
+    printf("Do You Want to Add a Node? 0 for No 1 for Yes\n");
+    scanf("%d",&input);
+
+    if(!input)
+    {
+        return NULL;
+    }
+    else
+    {
+        struct node * new_node = (struct node *) malloc(sizeof(struct node));
+        printf("Enter the Data: ");
+        scanf("%d",&data);
+
+        new_node->data = data;
+
+        printf("Left Child of %d\n",new_node->data);
+        new_node->left = create_tree();
+
+        printf("Right Child of %d\n",new_node->data);
+        new_node->right = create_tree();
+
+        return new_node;
+
+    }
+};
+
+int main()
+{
+    struct node *root = create_tree();
+    printf("%d ",root -> data);
+    printf("%d ",root->left->data);
+    printf("%d ",root->right->data);
+}
