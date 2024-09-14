f42377583719ed741ab82dd82c33c4a20a395116
Shohanur Rahman Shohan <sohanurhimel@gmail.com>
2024-09-14 13:13:00 +0600

    Create minimum_element_BST.cpp

---
+++ Tree/Binary_search_Tree/minimum_element_BST.cpp
@@ -0,0 +1,60 @@
+
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    struct node *left;
+    int data;
+    struct node *right;
+};
+
+struct node *create_tree()
+{
+    int input,data;
+    printf("Do You Want to Add a Node? 0 for No 1 for Yes\n");
+    scanf("%d",&input);
+
+    if(!input)
+    {
+        return NULL;
+    }
+    else
+    {
+        struct node * new_node = (struct node *) malloc(sizeof(struct node));
+        printf("Enter the Data: ");
+        scanf("%d",&data);
+
+        new_node->data = data;
+
+        printf("Left Child of %d\n",new_node->data);
+        new_node->left = create_tree();
+
+        printf("Right Child of %d\n",new_node->data);
+        new_node->right = create_tree();
+
+        return new_node;
+
+    }
+};
+
+struct node * minimum_element(struct node * root)
+{
+    if(root -> left == NULL)
+        return root;
+
+    return minimum_element (root -> left);
+
+};
+
+int main()
+{
+    struct node *root = create_tree();
+
+    struct node * temp = minimum_element(root);
+        printf("The Element is %d\n",temp -> data);
+
+}
+
